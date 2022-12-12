09aed1538a69cf98c661d8c407d4973fe6d679e1
xushaokui <wooshaokui@gmail.com>
2022-12-12 14:04:07 +0800

    新增语法高亮插件 TreeSitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -68,6 +68,8 @@ return packer.startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use "ahmedkhalf/project.nvim"
+  -- TreeSitter
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
